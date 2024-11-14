5514b9460906dfe3baa1c672607dd79e66123733
Nick Midmore <nmidmore@gmail.com>
2024-11-14 15:23:08 +0000

    add IEdible

--- Challenges/BoardGame.cs
+++ Challenges/BoardGame.cs
@@ -6,10 +6,15 @@ using System.Threading.Tasks;
 
 namespace Challenges
 {
-    public class BoardGame : Item
+    public class BoardGame : Item, IEdible
     {
         public BoardGame(User seller, string name, int price, string description) : base(seller, name, price, description)
         {
+        }
+
+        public void Eat(User user)
+        {
+            Console.WriteLine($"{user.Username} has eaten {Name}");
         }
     }
 }
--- Challenges/Food.cs
+++ Challenges/Food.cs
@@ -6,10 +6,15 @@ using System.Threading.Tasks;
 
 namespace Challenges
 {
-    public class Food : Item
+    public class Food : Item, IEdible
     {
         public Food(User seller, string name, int price, string description) : base(seller, name, price, description)
         {
+        }
+
+        public void Eat(User user)
+        {
+            Console.WriteLine($"{user.Username} has eaten {Name}");
         }
     }
 }
